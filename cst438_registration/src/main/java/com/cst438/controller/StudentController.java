7875fdfa12e11e7971ed44e663d8d78dd4f85756
Kenrick Hlaing <kenrick0102@gmail.com>
2023-09-18 13:58:11 -0700

    Finished Story 1, Add/Delete/Update/List Students

---
+++ cst438_registration/src/main/java/com/cst438/controller/StudentController.java
@@ -0,0 +1,86 @@
+package com.cst438.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import com.cst438.domain.CourseRepository;
+import com.cst438.domain.Enrollment;
+import com.cst438.domain.EnrollmentRepository;
+import com.cst438.domain.Student;
+import com.cst438.domain.StudentDTO;
+import com.cst438.domain.StudentRepository;
+import com.cst438.service.GradebookService;
+
+@RestController
+public class StudentController {
+	@Autowired
+	CourseRepository courseRepository;
+	
+	@Autowired
+	StudentRepository studentRepository;
+	
+	@Autowired
+	EnrollmentRepository enrollmentRepository;
+	
+	@Autowired
+	GradebookService gradebookService;
+	
+	// Adds a student
+	@PostMapping("/student")
+	@Transactional
+	public Student addStudent( @RequestBody StudentDTO studentDto ) {
+		Student newStudent = new Student();
+		newStudent.setName(studentDto.name());
+		newStudent.setEmail(studentDto.email());
+		newStudent.setStatusCode(studentDto.statusCode());
+		newStudent.setStatus(studentDto.status());
+		return studentRepository.save(newStudent);
+	}
+	
+	// Deletes a student
+	@DeleteMapping("/student/{student_id}")
+	@Transactional
+	public boolean deleteStudent(  @PathVariable int student_id ) {
+		List<Enrollment> studentEnrollments = enrollmentRepository.findAllByStudentId(student_id);
+		for (Enrollment enrollment : studentEnrollments) {
+	        if (enrollmentRepository.existsById(enrollment.getEnrollment_id())) {
+	            enrollmentRepository.deleteById(enrollment.getEnrollment_id());
+	        }
+	    }
+		if (studentRepository.existsById(student_id)) {
+            studentRepository.deleteById(student_id);
+            return true;
+        }
+        return false;
+	}
+	
+	// Updates a student
+	@PutMapping("/student/{student_id}/status")
+	@Transactional
+	public boolean deleteStudent(  @PathVariable int student_id, @RequestParam String newStatus  ) {
+		Student student = studentRepository.findById(student_id).orElse(null);
+		if (student != null) {
+            student.setStatus(newStatus);
+            studentRepository.save(student);
+            return true;
+        } else {
+            return false;
+        }
+	}
+	
+	// Lists all students
+	@GetMapping("/students")
+    public List<Student> getAllStudents() {
+        List<Student> students = (List<Student>) studentRepository.findAll();
+        return students;
+    }
+}
